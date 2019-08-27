adb68d96e0a538e529c68424bce5a98271e83081
Ashwin-9227 <46923234+Ashwin-9227@users.noreply.github.com>
2019-08-27 17:05:34 +0530

    Spring Boot MVC

---
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryController.java
@@ -0,0 +1,64 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/time-entries")
+public class TimeEntryController
+{
+
+    private TimeEntryRepository timeEntriesRepo;
+
+    public TimeEntryController(TimeEntryRepository timeEntriesRepo) {
+        this.timeEntriesRepo = timeEntriesRepo;
+    }
+
+    @PostMapping
+    public ResponseEntity create(@RequestBody TimeEntry timeEntryToCreate)
+    {
+        return new ResponseEntity<>(timeEntriesRepo.create(timeEntryToCreate), HttpStatus.CREATED);
+
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<TimeEntry> read(@PathVariable long timeEntryId)
+    {
+        if(timeEntriesRepo.find(timeEntryId) != null) {
+            return new ResponseEntity<>(timeEntriesRepo.find(timeEntryId), HttpStatus.OK);
+        }
+        else
+        {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TimeEntry>> list()
+    {
+        return new ResponseEntity<>(timeEntriesRepo.list(), HttpStatus.OK);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity update(@PathVariable long id, @RequestBody TimeEntry expected)
+    {
+        if(timeEntriesRepo.update(id,expected) != null) {
+            return new ResponseEntity<>(timeEntriesRepo.update(id,expected), HttpStatus.OK);
+        }
+        else
+        {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity delete(@PathVariable long id)
+    {
+        timeEntriesRepo.delete(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
